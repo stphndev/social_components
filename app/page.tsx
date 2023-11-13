c39189fa998e51aefbabbdc4f7703cb994b432af
braincee <kwesi938@gmail.com>
2023-11-13 12:26:39 +0000

    Slack Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Box } from '@mui/joy'
 import TelegramMessage from '@/components/TelegramMessage'
+import SlackMessage from '@components/SlackMessage'
 
 export default function Home() {
   return (
     <main>
       <Box sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>
         <TelegramMessage />
+        <SlackMessage />
       </Box>
     </main>
   )
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      dangerouslyAllowSVG: true,
+      contentDispositionType: 'attachment',
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    },
+  }
+  
+  module.exports = nextConfig
+  
